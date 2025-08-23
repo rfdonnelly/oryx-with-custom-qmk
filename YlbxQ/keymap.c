d658d33297a05e4968c595d12e330aec9c8d2731
Rob Donnelly <rfdonnelly@gmail.com>
2025-08-23 00:44:01 +0000

    feat: add default config for sm_td

--- YlbxQ/keymap.c
+++ YlbxQ/keymap.c
@@ -158,3 +158,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
 }
 
 
+smtd_resolution on_smtd_action(uint16_t keycode, smtd_action action, uint8_t tap_count) {
+    switch (keycode) {
+        SMTD_MT(KC_A, KC_LEFT_GUI)
+        SMTD_MT(KC_S, KC_LEFT_ALT)
+        SMTD_MT(KC_D, KC_LEFT_CTRL)
+        SMTD_MT(KC_F, KC_LSFT)
+    }
+
+    return SMTD_RESOLUTION_UNHANDLED;
+}
